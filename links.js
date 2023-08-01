3acf75ac5f5523efe3dbaee1a793fbb310f2e1df
Martinek16 <84472276+Martinek16@users.noreply.github.com>
2023-08-01 17:06:34 +0200

    Add files via upload

---
+++ links.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function () {
+  showSavedLinks();
+});
+
+function showSavedLinks() {
+  var savedPages = JSON.parse(localStorage.getItem("savedPages")) || [];
+  var linksContainer = document.getElementById("linksContainer");
+  linksContainer.innerHTML = "";
+
+  // Group saved links by categories
+  var groupedLinks = {};
+  for (var i = 0; i < savedPages.length; i++) {
+      var page = savedPages[i];
+      var category = page.category || "Uncategorized"; // Set default category as "Uncategorized" if no category is provided
+      if (!groupedLinks[category]) {
+          groupedLinks[category] = [];
+      }
+      groupedLinks[category].push(page);
+  }
+
+  for (var category in groupedLinks) {
+      var categoryHeading = document.createElement("h2");
+      categoryHeading.textContent = category;
+
+      var linksList = document.createElement("ul");
+      var linksInCategory = groupedLinks[category];
+      for (var j = 0; j < linksInCategory.length; j++) {
+          var link = linksInCategory[j];
+          var listItem = document.createElement("li");
+          var card = document.createElement("div"); // Create a card element
+          card.className = "card"; // Add the "card" class to the card element
+
+          var anchor = document.createElement("a");
+          anchor.href = link.url;
+          anchor.target = "_blank"; // Set the target attribute to "_blank"
+          anchor.textContent = link.title;
+
+          var ellipsisIcon = document.createElement("i"); // Create the <i> element for the ellipsis icon
+          ellipsisIcon.className = "fa-solid fa-ellipsis"; // Set the classes for the Font Awesome ellipsis icon
+
+          card.appendChild(anchor); // Append the anchor to the card element
+          card.appendChild(ellipsisIcon); // Append the ellipsis icon to the card element
+          listItem.appendChild(card); // Append the card to the list item
+          linksList.appendChild(listItem);
+      }
+
+      var categoryColumn = document.createElement("div");
+      categoryColumn.className = "category-column";
+      categoryColumn.appendChild(categoryHeading);
+      categoryColumn.appendChild(linksList);
+      linksContainer.appendChild(categoryColumn);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSavedLinks();
+  setupIconClick();
+});
+
+function setupIconClick() {
+  var cards = document.querySelectorAll(".card"); // Select all card elements directly
+
+  var modal = document.getElementById("myModal");
+  var modalTitleInput = document.getElementById("modalTitle");
+  var modalUrlInput = document.getElementById("modalUrl");
+
+  cards.forEach(function (card) {
+    var icon = card.querySelector("i.fa-ellipsis"); // Find the ellipsis icon within the card
+    card.addEventListener("click", function () {
+      var savedPages = JSON.parse(localStorage.getItem("savedPages")) || [];
+      var cardTitle = card.querySelector("a").textContent; // Get the title of the clicked card
+
+      // Find the corresponding data in the savedPages array based on the title
+      var clickedCardData = savedPages.find(function (page) {
+        return page.title === cardTitle;
+      });
+
+      if (clickedCardData) {
+        // Set the input fields with the data from the clicked card
+        modalTitleInput.value = clickedCardData.title;
+        modalUrlInput.value = clickedCardData.url;
+
+        modal.style.display = "flex";
+      }
+    });
+  });
+
+  // Close the modal when clicking outside the content area
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
+
+
--- script.js
+++ script.js
@@ -1,50 +1,80 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var savePageBtn = document.getElementById("savePageBtn");
-    savePageBtn.addEventListener("click", function () {
+  var savePageBtn = document.getElementById("savePageBtn");
+  savePageBtn.addEventListener("click", function () {
+    var selectedCategory = getSelectedCategory();
+    if (selectedCategory) {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
         var pageUrl = activeTab.url;
         var pageTitle = activeTab.title;
-        savePage(pageUrl, pageTitle);
+        savePage(pageUrl, pageTitle, selectedCategory);
       });
-    });
-  
-    var openLinksBtn = document.getElementById("openLinksBtn");
-    openLinksBtn.addEventListener("click", function () {
-      openLinksPage();
-    });
-  
-    function savePage(url, title) {
-      var savedPages = JSON.parse(localStorage.getItem("savedPages")) || [];
-      var isPageSaved = savedPages.some(function (page) {
-        return page.url === url;
-      });
-  
-      if (!isPageSaved) {
-        savedPages.push({ url: url, title: title });
-        localStorage.setItem("savedPages", JSON.stringify(savedPages));
-  
-        var messageDiv = document.getElementById("message");
-        messageDiv.textContent = "Stran je shranjena!";
-      } else {
-        var messageDiv = document.getElementById("message");
-        messageDiv.textContent = "Stran je že shranjena!";
-      }
-    }
-  
-    function openLinksPage() {
-      var savedPages = JSON.parse(localStorage.getItem("savedPages")) || [];
-      var linksPageContent = "<h1>Shranjene povezave:</h1><ul>";
-  
-      for (var i = 0; i < savedPages.length; i++) {
-        var page = savedPages[i];
-        linksPageContent += '<li><a href="' + page.url + '">' + page.title + '</a></li>';
-      }
-  
-      linksPageContent += "</ul>";
-  
-      var linksPage = window.open("", "_blank");
-      linksPage.document.write(linksPageContent);
+    } else {
+      var messageDiv = document.getElementById("message");
+      messageDiv.textContent = "Please select a category before saving!";
     }
   });
-  
+
+  function getSelectedCategory() {
+    var checkboxes = document.getElementsByName("categoryCheckbox");
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        return checkboxes[i].value;
+      }
+    }
+    return null;
+  }
+
+  function savePage(url, title, category) {
+    var savedPages = JSON.parse(localStorage.getItem("savedPages")) || [];
+    var isPageSaved = savedPages.some(function (page) {
+      return page.url === url;
+    });
+
+    if (!isPageSaved) {
+      savedPages.push({ url: url, title: title, category: category });
+      localStorage.setItem("savedPages", JSON.stringify(savedPages));
+
+      var messageDiv = document.getElementById("message");
+      messageDiv.textContent = "Saved!";
+    } else {
+      var messageDiv = document.getElementById("message");
+      messageDiv.textContent = "The page has already been saved!";
+    }
+  }
+
+  function openLinksPage() {
+    var savedPages = JSON.parse(localStorage.getItem("savedPages")) || [];
+    var linksPageContent = "<h1>Saved links:</h1>";
+
+    // Group saved links by categories
+    var groupedLinks = {};
+    for (var i = 0; i < savedPages.length; i++) {
+      var page = savedPages[i];
+      if (!groupedLinks[page.category]) {
+        groupedLinks[page.category] = [];
+      }
+      groupedLinks[page.category].push(page);
+    }
+
+    for (var category in groupedLinks) {
+      linksPageContent += "<h2>" + category + "</h2><ul>";
+      var linksInCategory = groupedLinks[category];
+      for (var j = 0; j < linksInCategory.length; j++) {
+        var link = linksInCategory[j];
+        linksPageContent +=
+          '<li><a href="' + link.url + '">' + link.title + '</a></li>';
+      }
+      linksPageContent += "</ul>";
+    }
+
+    var linksPage = window.open("", "_blank");
+    linksPage.document.write(linksPageContent);
+  }
+
+  var openLinksBtn = document.getElementById("openLinksBtn");
+      openLinksBtn.addEventListener("click", function () {
+        var linksPageURL = chrome.runtime.getURL("links.html");
+        window.open(linksPageURL, "_blank");
+      });
+});
