d7b977bdc6d3d1e605a06c5e84ecdde1b4e6d94c
Martinek16 <martinek.miha@gmail.com>
2023-11-19 16:16:12 +0100

    2.13

--- links.js
+++ links.js
@@ -3,68 +3,70 @@ document.addEventListener("DOMContentLoaded", () => {
   setupModalClose();
   setupSettingsButton();
   initializeCategories();
-    displayCategories();
+  displayCategories();
 });
 
+//-------------------------------------------------------------------------------------- 
+// Deafult - showSavedLinks, 
+//-------------------------------------------------------------------------------------- 
 function showSavedLinks() {
   const savedPages = JSON.parse(localStorage.getItem('savedPages')) || [];
   const linksContainer = document.getElementById('linksContainer');
   linksContainer.innerHTML = '';
 
-  // Group saved links by categories
   const groupedLinks = savedPages.reduce((acc, page) => {
-    const category = page.category || 'Uncategorized';
-    acc[category] = acc[category] || [];
-    acc[category].push(page);
-    return acc;
+      const category = page.category || 'Uncategorized';
+      acc[category] = acc[category] || [];
+      acc[category].push(page);
+      return acc;
   }, {});
 
   for (const category in groupedLinks) {
-    const categoryHeading = document.createElement('h2');
-    categoryHeading.textContent = category;
+      const categoryHeading = document.createElement('h2');
+      categoryHeading.textContent = category;
 
-    const linksList = document.createElement('ul');
-    linksList.style.listStyleType = 'none';
+      const linksList = document.createElement('ul');
+      linksList.style.listStyleType = 'none';
 
-    groupedLinks[category].forEach(link => {
-      const listItem = document.createElement('li');
-      const card = document.createElement('div');
-      card.className = 'card';
-      card.draggable = true;
-      card.addEventListener('dragstart', dragStart);
-      card.addEventListener('dragend', dragEnd);
+      groupedLinks[category].forEach(link => {
+          const listItem = document.createElement('li');
+          const card = document.createElement('div');
+          card.className = 'card';
+          card.draggable = true;
+          card.addEventListener('dragstart', dragStart);
+          card.addEventListener('dragend', dragEnd);
 
-      const anchor = document.createElement('a');
-      anchor.href = link.url;
-      anchor.target = '_blank';
-      anchor.textContent = link.title;
+          const anchor = document.createElement('a');
+          anchor.href = link.url;
+          anchor.target = '_blank';
+          anchor.textContent = link.title;
 
-      const ellipsisIcon = document.createElement('i');
-      ellipsisIcon.className = 'fa-solid fa-ellipsis';
-      ellipsisIcon.addEventListener('click', (e) => {
-        e.stopPropagation();
-        showEditModal(link);
+          const ellipsisIcon = document.createElement('i');
+          ellipsisIcon.className = 'fa-solid fa-ellipsis';
+          ellipsisIcon.addEventListener('click', (e) => {
+              e.stopPropagation();
+              showEditModal(link);
+          });
+
+          card.appendChild(anchor);
+          card.appendChild(ellipsisIcon);
+          listItem.appendChild(card);
+          linksList.appendChild(listItem);
       });
 
-      card.appendChild(anchor);
-      card.appendChild(ellipsisIcon);
-      listItem.appendChild(card);
-      linksList.appendChild(listItem);
-    });
-
-    const categoryColumn = document.createElement('div');
-    categoryColumn.className = 'category-column';
-    categoryColumn.appendChild(categoryHeading);
-    categoryColumn.appendChild(linksList);
-    linksContainer.appendChild(categoryColumn);
-    categoryColumn.addEventListener('dragover', dragOver);
-    categoryColumn.addEventListener('dragenter', dragEnter);
-    categoryColumn.addEventListener('dragleave', dragLeave);
-    categoryColumn.addEventListener('drop', dragDrop);
+      const categoryColumn = document.createElement('div');
+      categoryColumn.className = 'category-column';
+      categoryColumn.appendChild(categoryHeading);
+      categoryColumn.appendChild(linksList);
+      linksContainer.appendChild(categoryColumn);
+      categoryColumn.addEventListener('dragover', dragOver);
+      categoryColumn.addEventListener('dragenter', dragEnter);
+      categoryColumn.addEventListener('dragleave', dragLeave);
+      categoryColumn.addEventListener('drop', dragDrop);
   }
 }
 
-
+//
 function showEditModal(link) {
   const modal = document.getElementById('myModal');
   const modalTitleInput = document.getElementById('modalTitle');
@@ -86,10 +88,10 @@ function updateLink(originalLink, newTitle, newUrl) {
   const savedPages = JSON.parse(localStorage.getItem('savedPages')) || [];
   const pageIndex = savedPages.findIndex(page => page.url === originalLink.url);
   if (pageIndex !== -1) {
-    savedPages[pageIndex] = { ...originalLink, title: newTitle, url: newUrl };
-    localStorage.setItem('savedPages', JSON.stringify(savedPages));
-    closeEditModal();
-    showSavedLinks();
+      savedPages[pageIndex] = { ...originalLink, title: newTitle, url: newUrl };
+      localStorage.setItem('savedPages', JSON.stringify(savedPages));
+      closeEditModal();
+      showSavedLinks();
   }
 }
 
@@ -109,9 +111,9 @@ function closeEditModal() {
 function setupModalClose() {
   const modal = document.getElementById('myModal');
   window.onclick = (event) => {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
+      if (event.target === modal) {
+          modal.style.display = 'none';
+      }
   };
 }
 
@@ -133,23 +135,26 @@ function displayCategories() {
 function addNewCategory() {
   const newCategoryName = document.getElementById('newCategoryName').value;
   if (newCategoryName) {
-    const categories = JSON.parse(localStorage.getItem('categories')) || ['Work', 'Other', 'Fun', 'Uncategorized'];
-    if (!categories.includes(newCategoryName)) {
-      categories.push(newCategoryName);
-      localStorage.setItem('categories', JSON.stringify(categories));
-      displayCategories();
-    }
+      const categories = JSON.parse(localStorage.getItem('categories')) || ['Work', 'Other', 'Fun', 'Uncategorized'];
+      if (!categories.includes(newCategoryName)) {
+          categories.push(newCategoryName);
+          localStorage.setItem('categories', JSON.stringify(categories));
+          displayCategories();
+      }
   }
 }
 
-// Drag and Drop funkcije
+
+
+//-------------------------------------------------------------------------------------- 
+// Drag and drop 
+//-------------------------------------------------------------------------------------- 
 function dragStart() {
   this.classList.add('dragging');
 }
 
 function dragEnd() {
   this.classList.remove('dragging');
-  // Tukaj dodajte logiko za posodobitev kategorije kartice
 }
 
 function dragOver(e) {
@@ -175,12 +180,15 @@ function dragDrop() {
   updateCategory(linkTitle, newCategory);
 }
 
+//-------------------------------------------------------------------------------------- 
+// Category 
+//-------------------------------------------------------------------------------------- 
 function updateCategory(title, newCategory) {
   const savedPages = JSON.parse(localStorage.getItem('savedPages')) || [];
   const pageIndex = savedPages.findIndex(page => page.title === title);
   if (pageIndex !== -1) {
-    savedPages[pageIndex].category = newCategory;
-    localStorage.setItem('savedPages', JSON.stringify(savedPages));
+      savedPages[pageIndex].category = newCategory;
+      localStorage.setItem('savedPages', JSON.stringify(savedPages));
   }
 }
 
@@ -193,17 +201,14 @@ function initializeCategories() {
 
 function displayCategories() {
   const categories = getCategories();
-  const categoriesContainer = document.getElementById('categoriesContainer'); // Predpostavljamo, da imate ta element v HTML
+  const categoriesContainer = document.getElementById('categoriesContainer');
   categoriesContainer.innerHTML = '';
 
   categories.forEach(category => {
       const categoryElement = document.createElement('div');
       categoryElement.textContent = category;
-      // Dodajte tukaj elemente za urejanje ali brisanje, če so potrebni
       categoriesContainer.appendChild(categoryElement);
   });
-
-  // Dodajte tukaj funkcionalnost za dodajanje novih kategorij
 }
 
 function addNewCategory(categoryName) {
@@ -211,7 +216,7 @@ function addNewCategory(categoryName) {
   if (categories.indexOf(categoryName) === -1) {
       categories.push(categoryName);
       saveCategories(categories);
-      displayCategories(); // Ponovno prikažite kategorije
+      displayCategories();
   } else {
       alert("Category already exists.");
   }
